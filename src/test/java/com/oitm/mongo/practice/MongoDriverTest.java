8b9575bee3a58337d230c6a98b029dec6edcb0d6
songshuran <happy_ran@outlook.com>
2019-03-30 15:31:10 +0800

    更新，删除操作

--- src/test/java/com/oitm/mongo/practice/MongoDriverTest.java
+++ src/test/java/com/oitm/mongo/practice/MongoDriverTest.java
@@ -2,8 +2,12 @@ package com.oitm.mongo.practice;
 
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
+import com.mongodb.client.result.DeleteResult;
+import com.mongodb.client.result.UpdateResult;
+import com.oitm.mongo.practice.constants.MongoConstants;
 import com.oitm.mongo.practice.entity.Article;
 import org.bson.Document;
+import org.bson.types.ObjectId;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,13 +25,13 @@ import static com.mongodb.client.model.Filters.*;
 public class MongoDriverTest {
 
     /**
-     *     private String title;
-     *     private String url;
-     *     private String author;
-     *     private List<String> tags;
-     *     private Long visitCount;
-     *     private Date ctime;
-     *     private Date mtime;
+     * private String title;
+     * private String url;
+     * private String author;
+     * private List<String> tags;
+     * private Long visitCount;
+     * private Date ctime;
+     * private Date mtime;
      */
 
     public static final String ARTICLE_CLASS = "com.oitm.mongo.practice.entity.Article";
@@ -36,7 +40,7 @@ public class MongoDriverTest {
     private MongoCollection<Document> articleCollection;
 
     @Test
-    public void testInsert(){
+    public void testInsert() {
         ArrayList<String> tags = new ArrayList<>();
         tags.add("java");
         tags.add("mongo");
@@ -51,7 +55,7 @@ public class MongoDriverTest {
     }
 
     @Test
-    public void testQuery(){
+    public void testQuery() {
         // 查询所有
 //        FindIterable<Document> documents = articleCollection.find();
 //        MongoCursor<Document> cursor = documents.iterator();
@@ -84,16 +88,59 @@ public class MongoDriverTest {
 //        }
 
         //OR查询
-        MongoCursor<Document> cursor = articleCollection.find().filter(or(eq(Article.TITLE, "Oitm1"), eq(Article.TITLE, "Oitm2"))).iterator();
-        while (cursor.hasNext()) {
-            Document document5 = cursor.next();
-            System.out.println(document5);
+//        MongoCursor<Document> cursor = articleCollection.find().filter(or(eq(Article.TITLE, "Oitm1"), eq(Article.TITLE, "Oitm2"))).iterator();
+//        while (cursor.hasNext()) {
+//            Document document5 = cursor.next();
+//            System.out.println(document5);
+//        }
+
+        // sort
+//        MongoCursor<Document> iterator = articleCollection.find().sort(new Document(Article.TITLE, -1)).iterator();
+//        while (iterator.hasNext()) {
+//            Document document8 = iterator.next();
+//            System.out.println(document8);
+//        }
+
+        //分页
+        MongoCursor<Document> iterator = articleCollection.find().skip(3).limit(2).iterator();
+        while (iterator.hasNext()) {
+            Document document9 = iterator.next();
+            System.out.println(document9);
         }
 
         //关联查询   此处只是举例子
 //        Document document6 = new Document("fieldName", new DBRef("collection", "id"));
 //        Document document7 = articleCollection.find(document6).first();
 //        System.out.println(document7);
+
+    }
+
+
+    @Test
+    public void testUpdate() {
+        //更新对应id的article的title内容, 注意 `MongoConstants.MONGO_SET` 设置
+//        String articleID = "5c9ddd2f3d428cc67aed0598";
+//        UpdateResult update = articleCollection.updateOne(eq(MongoConstants.MONGO_ID, new ObjectId(articleID)),
+//                new Document(MongoConstants.MONGO_SET, new Document(Article.TITLE, "Oitm_Driver_Update")));
+//        System.out.println(update.getModifiedCount());
+
+        // 更新多个同时都满足条件的Document
+        UpdateResult updateResult = articleCollection.updateMany(eq(Article.TITLE, "Oitm_Update_Third"),
+                new Document(MongoConstants.MONGO_SET, new Document(Article.TITLE, "Oitm_Driver_Update_Third")));
+        System.out.println(updateResult.getModifiedCount());
+
+    }
+
+    @Test
+    public void testDelete() {
+        //根据id进行删除
+//        String articleID = "5c9f1782678228f290b64691";
+//        DeleteResult deleteResult = articleCollection.deleteOne(eq(MongoConstants.MONGO_ID, new ObjectId(articleID)));
+//        System.out.println(deleteResult.getDeletedCount());
+
+        //删除所有满足条件的Document
+        DeleteResult result = articleCollection.deleteMany(eq(Article.TITLE, "Oitm"));
+        System.out.println(result.getDeletedCount());
 
     }
 
